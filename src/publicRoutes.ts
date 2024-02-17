ccda3a3d5e9b45b4a44f0591209dd078df2350a8
assem12345assem <107938154+assem12345assem@users.noreply.github.com>
2024-02-17 20:46:17 +0600

    merge_conflict_resolution_02

--- src/publicRoutes.ts
+++ src/publicRoutes.ts
@@ -3,7 +3,8 @@ import { signIn, signUp } from "./controllers/userControllers";
 import {getAcademicAreas} from "./controllers/academicAreaControllers.ts";
 import {courses} from "./controllers/courseControllers";
 import {getReview, getReviews} from "./controllers/reviewControllers.ts";
-import {contactServices} from "./controllers/studentControllers/profileManagementControllers.ts";
+import {courseRoutesPublic} from "./controllers/courseControllers/courseRoutesPublic.ts";
+import {contactServices} from "./controllers/profileControllers/contactsManagementControllers.ts";
 
 let router = express.Router()
 
@@ -11,7 +12,7 @@ router.post('/sign-up', signUp)
 router.post('/sign-in', signIn)
 
 router.get('/academicAreas', getAcademicAreas)
-router.get('/courses', courses)
+router.use('', courseRoutesPublic)
 router.get('/reviews', getReviews)
 router.get('/reviews/:id', getReview)
 //Instagram, Email, WeChat:
